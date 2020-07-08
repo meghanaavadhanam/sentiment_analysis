0b18044fc100a083edb220dbf668282fbeb0c97f
meghanaavadhanam <36404872+meghanaavadhanam@users.noreply.github.com>
2020-07-08 14:09:18 +0530

    Add files via upload

---
+++ SentimentTest.py
@@ -0,0 +1,9 @@
+import SentimentModel as s
+
+##print(s.sentiment("This movie was awesome! The acting was great, plot was wonderful, and there were pythons...so yea!"))
+##print(s.sentiment("This movie was utter junk. There were absolutely 0 pythons. I don't see what the point was at all. Horrible movie, 0/10"))
+
+print(s.sentiment("TCS results were good. generated positive growth. all verticals grew. Europe did very well"))
+print(s.sentiment("HCL disappointed the street with mere small growth rate. they did not meet the guidance and forecast is below expectations"))
+print(s.sentiment("Wipro has failed to live up street expectations even though there are some brightspots"))
+
